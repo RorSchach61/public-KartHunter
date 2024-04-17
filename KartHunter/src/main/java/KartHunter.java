699e3672d2bb20cfa1fa6390e5e5f7c7cd5ae1e7
RorSchach61 <155496040+RorSchach61@users.noreply.github.com>
2024-04-17 09:53:06 -0700

    Public version

---
+++ KartHunter/src/main/java/KartHunter.java
@@ -0,0 +1,12 @@
+package io.magicthegathering.javasdk.api;
+
+import io.magicthegathering.javasdk.resource.Card;
+
+public class KartHunter
+{
+public static void main(String[] args)
+{
+
+ boolean False = new boolean;
+}
+}
